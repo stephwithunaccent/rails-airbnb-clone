c943014492bdf8c6a097af302b19fc12b65a1642
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-14 16:53:00 +0200

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
