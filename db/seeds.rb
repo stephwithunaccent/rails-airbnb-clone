42bd427aa0bd105db891aad79f776011848f77c5
Leopold Pacquement <leopoldpacquement@gmail.com>
2017-08-17 16:58:14 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@ Participation.destroy_all
 puts "Filling seed"
 
 puts "generating seed"
-jerome = User.create({email: "jerome@gmail.com", password: "123soleil", name: 'Jerome & Arnaud', sex: 'Male & Male'})
-lea = User.create({email: "lea@gmail.com", password: "123soleil", name: 'Lea & Mathieu', sex: 'Female & Female'})
-leo = User.create({email: "leo@gmail.com", password: "123soleil", name: 'Kim & Leo', sex: 'Male & Female'})
+jerome = User.create({email: "jerome@gmail.com", password: "123soleil", name: 'Jerome & Arnaud', sex: '⚣ Gay'})
+lea = User.create({email: "lea@gmail.com", password: "123soleil", name: 'Lea & Mathieu', sex: 'Lesbian'})
+leo = User.create({email: "leo@gmail.com", password: "123soleil", name: 'Kim & Leo', sex: '⚤ Straight'})
 
 event1 = Event.create({user: leo, start_at: "2018-05-15 15:37:33 +0200", description: "Un peu de plaisir n'a jamais tué personne... Jeu de main jeu de vilain...", address: 'Paris', title:'Les Jambes Attachées'})
 participation_1 = Participation.new
 participation_1.user = jerome
 participation_1.event = event1
+participation_1.rating = 7
+participation_1.review = "Very nice night in a very nice place. They are both very hot!"
 participation_1.save
 
 event2 = Event.create({user: leo, start_at: "2018-06-15 15:37:33 +0200", description: "Hey, we love sex and violence. We are welcoming free lancers of pur pleasure", address: 'Londres', title:'Fucking & Punshing'})
@@ -33,6 +35,7 @@ participation_4 = Participation.new
 participation_4.user = leo
 participation_4.event = event4
 participation_4.save
+
 
 puts "Seed complete"
 
