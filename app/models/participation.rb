bae21e81f33bbbe559f38b79bde7bef5ad3a558d
Stephanie Smith <stephaniecatherine.smith@gmail.com>
2017-08-18 16:16:38 +0200

    Participations page fixed

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,4 +2,7 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
   validates :rating, :numericality => {:only_integer => true, allow_nil: true}
+
+  scope :upcoming, -> { joins(:event).where("events.start_at > ?", Time.now) }
+  scope :past,     -> { joins(:event).where("events.start_at < ?", Time.now) }
 end
