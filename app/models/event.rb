2d2a5688c5e59dee6d8123127e839f82889d1329
Romain Thoreau <romain.thoreau@outlook.fr>
2017-08-18 14:49:29 +0200

    updating seed and atachment model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
-class Event < ApplicationRecord
+  class Event < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
   belongs_to :user
   has_many :participations, dependent: :destroy
 
-  has_attachments :photo
+  has_attachment :photo
   validates :title, :address, :description, :start_at, presence: true
 end
